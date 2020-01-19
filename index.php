a7673bd1ae5235ae4ac8f031372997da61484d9a
Luiz Eduardo <luizeof@gmail.com>
2020-01-18 21:05:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ use mikehaertl\shellcommand\Command;
 require 'vendor/autoload.php';
 
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+
 header("Cache-Control: post-check=0, pre-check=0", false);
+
 header("Pragma: no-cache");
 
 $params = $_REQUEST;
